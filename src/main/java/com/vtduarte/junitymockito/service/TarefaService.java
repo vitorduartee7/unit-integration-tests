db08b8d7fba647e8341851400476c3a32e0cce1a
Vitor <vtduarte2007@gmail.com>
2026-09-17 05:31:20 -0300

    add instancia do repository

--- src/main/java/com/vtduarte/junitymockito/service/TarefaService.java
+++ src/main/java/com/vtduarte/junitymockito/service/TarefaService.java
@@ -1,12 +1,20 @@
 package com.vtduarte.junitymockito.service;
 
+import com.vtduarte.junitymockito.exception.ResourceNotFoundException;
 import com.vtduarte.junitymockito.model.PrioridadeTarefaEnum;
 import com.vtduarte.junitymockito.model.StatusTarefaEnum;
 import com.vtduarte.junitymockito.model.TarefaEntity;
+import com.vtduarte.junitymockito.repository.TarefaRepository;
 
 import java.time.LocalDate;
 
 public class TarefaService {
+
+    private final TarefaRepository tarefaRepository;
+
+    public TarefaService(TarefaRepository tarefaRepository) {
+        this.tarefaRepository = tarefaRepository;
+    }
 
     public TarefaEntity criar(String titulo, String descricao, PrioridadeTarefaEnum prioridade, LocalDate dataVencimento) {
 
@@ -18,7 +26,14 @@ public class TarefaService {
             throw new IllegalArgumentException("Data de vencimento nao pode ser no passado");
         }
 
-        return new TarefaEntity(titulo, descricao, prioridade, dataVencimento);
+        var tarefa = new TarefaEntity(titulo, descricao, prioridade, dataVencimento);
+
+        return tarefaRepository.salvar(tarefa);
+    }
+
+    public TarefaEntity buscarPorId(Long idTarefa) {
+        return tarefaRepository.buscarPorId(idTarefa)
+                .orElseThrow(() -> new ResourceNotFoundException("Tarefa nao encontrada"));
     }
 
     public TarefaEntity atualizarStatus(TarefaEntity tarefa, StatusTarefaEnum novoStatus) {
