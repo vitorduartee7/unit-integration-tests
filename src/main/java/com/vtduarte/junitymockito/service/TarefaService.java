6e9d6843a4b109d0b79d97dbd4a599cdfe410849
Vitor <vtduarte2007@gmail.com>
2026-09-16 22:29:20 -0300

    add método atualizar status

--- src/main/java/com/vtduarte/junitymockito/service/TarefaService.java
+++ src/main/java/com/vtduarte/junitymockito/service/TarefaService.java
@@ -1,6 +1,7 @@
 package com.vtduarte.junitymockito.service;
 
 import com.vtduarte.junitymockito.model.PrioridadeTarefaEnum;
+import com.vtduarte.junitymockito.model.StatusTarefaEnum;
 import com.vtduarte.junitymockito.model.TarefaEntity;
 
 import java.time.LocalDate;
@@ -10,13 +11,42 @@ public class TarefaService {
     public TarefaEntity criar(String titulo, String descricao, PrioridadeTarefaEnum prioridade, LocalDate dataVencimento) {
 
         if (titulo == null || titulo.isBlank()) {
-            throw new IllegalArgumentException("Título é obrigatório");
+            throw new IllegalArgumentException("Titulo e obrigatorio");
         }
 
         if (dataVencimento != null && dataVencimento.isBefore(LocalDate.now())) {
-            throw new IllegalArgumentException("Data de vencimento não pode ser no passado");
+            throw new IllegalArgumentException("Data de vencimento nao pode ser no passado");
         }
 
         return new TarefaEntity(titulo, descricao, prioridade, dataVencimento);
+    }
+
+    public TarefaEntity atualizarStatus(TarefaEntity tarefa, StatusTarefaEnum novoStatus) {
+
+        StatusTarefaEnum statusAtual = tarefa.getStatus();
+
+        switch (statusAtual) {
+            case PENDENTE, CONCLUIDA:
+                if (novoStatus.equals(StatusTarefaEnum.EM_ANDAMENTO)) {
+                    tarefa.setStatus(StatusTarefaEnum.EM_ANDAMENTO);
+                } else {
+                    throw new IllegalArgumentException("Transição de " + statusAtual
+                            + " para " + novoStatus + " não é permitida");
+                }
+                break;
+
+            case EM_ANDAMENTO:
+                if (novoStatus.equals(StatusTarefaEnum.CONCLUIDA)) {
+                    tarefa.setStatus(StatusTarefaEnum.CONCLUIDA);
+                } else if (novoStatus.equals(StatusTarefaEnum.PENDENTE)) {
+                    tarefa.setStatus(StatusTarefaEnum.PENDENTE);
+                } else {
+                    throw new IllegalArgumentException("Transição de " + statusAtual
+                            + " para " + novoStatus + " não é permitida");
+                }
+                break;
+        }
+
+        return tarefa;
     }
 }
