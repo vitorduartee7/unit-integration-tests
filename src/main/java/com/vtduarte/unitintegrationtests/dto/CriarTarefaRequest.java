62c5ff01f2a85d15b6d825e2e1d112d9a6cef1c7
Vitor <vtduarte2007@gmail.com>
2026-09-22 05:19:36 -0300

    add dto CriarTarefaRequest

---
+++ src/main/java/com/vtduarte/unitintegrationtests/dto/CriarTarefaRequest.java
@@ -0,0 +1,15 @@
+package com.vtduarte.unitintegrationtests.dto;
+
+import com.vtduarte.unitintegrationtests.model.PrioridadeTarefaEnum;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+public record CriarTarefaRequest(
+        @NotBlank(message = "Título é obrigatório") String titulo,
+        String descricao,
+        @NotNull PrioridadeTarefaEnum prioridade,
+        @Future(message = "Data de vencimento deve ser futura") LocalDate dataVencimento
+) {}
